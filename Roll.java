83e72aaf484bc3f3a5e46a99d08e982d433080cd
Neua0114 <neua0114@my.uwstout.edu>
2017-11-30 13:55:59 -0600

    Add files via upload

---
+++ Roll.java
@@ -0,0 +1,10 @@
+
+public class Roll {
+    public static int Roller (int size) {
+        double random= (int) (Math.random()*50);
+        random=random +1;
+        int i= (int) random;
+        i = i % size;
+        return i;
+    }
+}
