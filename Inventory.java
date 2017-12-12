c219db82b1990a74c5c6be221df504648408789f
Neua0114 <neua0114@my.uwstout.edu>
2017-12-12 14:38:00 -0600

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -5,8 +5,8 @@ public class Inventory {
 	private int hpCount; 
 	private final String hp = "Health Potion";
 	//Ammunition
-	private int ammoCount;
-	private final String ammo = "Ammunition";
+	private int[] ammo = new int[3];
+	private String[] ammoS = {"Primary Ammo", "Secondary Ammo", "Tertiary Ammo"};
 	//Gold
 	private int goldCount;
 	private final String gold = "Gold";
@@ -14,7 +14,9 @@ public class Inventory {
 	//Default constructor
 	public Inventory() {
 		hpCount = 1;
-		ammoCount = 15;
+		ammo[0] = 0;
+		ammo[1] = 0;
+		ammo[2] = 0;
 		goldCount = 0;
 	}
 	
@@ -38,6 +40,84 @@ public class Inventory {
 	/**
 	 * @return The amount of health to heal
 	 */
+	public int heal() {
+		if (hpCount > 0) {
+			hpCount--;
+			return Roll.Roller(4) + Roll.Roller(4) + 2;
+		}
+		else {
+			System.out.println("No health potions remaining.");
+			return 0;
+		}
+	}
+	
+	//Add ammo to inventory
+	/**
+	 * @param value The amount of ammo to add to the inventory
+	 */
+	public void addAmmo(int type, int value) {
+		ammo[type%3]+=value;
+	}
+	
+	//Return quantity of ammo remaining
+	/**
+	 * @return The current amount of ammunition
+	 */
+	public int getAmmoCount(int type) {
+		return ammo[type%3];
+	}
+	
+	//Function to use ammo
+	
+	/**
+	 * @return True or false depending on if there is ammo to be fired
+	 */
+	public boolean hasAmmo(int type) {
+		if (ammo[type%3] > 0) {
+			return true;
+		}
+		else {
+			System.out.println("No ammo.");
+			return false;
+		}
+	}
+	public void useAmmo(int type) {
+		ammo[type%3]--;
+	}
+	
+	//Add gold to pouch
+	/**
+	 * @param value The amount of gold to add to the pouch
+	 */
+	public void addGold(int value) {
+		goldCount+=value;
+	}
+	
+	//Return current gold
+	/**
+	 * @return The amount of gold the charachter has
+	 */
+	public int getGoldCount() {
+		return goldCount;
+	}
+	
+	//Function to spend gold
+	/**
+	 * @param cost How much gold the character is trying to spend
+	 * @return Whether or not they character has enough gold
+	 */
+	public boolean spend(int cost) {
+		if (goldCount - cost >= 0) {
+			goldCount -= cost;
+			return true;
+		}
+		else {
+			System.out.println("Insufficient Funds.");
+			return false;
+		}
+	}
+
+}
 	public int heal() {
 		if (hpCount > 0) {
 			hpCount--;
