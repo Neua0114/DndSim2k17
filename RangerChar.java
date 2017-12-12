6582a1fd15fe946b6541f0fcc88761215bda8cc1
Neua0114 <neua0114@my.uwstout.edu>
2017-12-12 14:01:16 -0600

    Update RangerChar.java

--- RangerChar.java
+++ RangerChar.java
@@ -1,129 +1,123 @@
 
-public class RangerChar extends Creature {
-	
+public class RangerChar extends Creature{
 	//Instance Variables
-	int STR = 1;
-	int DEX = 3;
-	int CON = 1;
-	int INTEL = 0;
-	int WIS = 3;
-	int CHA = -1;
-	int MAXHEALTH = 11;
-	int AC = 15;
-	int HP = 11;
+		int STR = 1;
+		int DEX = 3;
+		int CON = 1;
+		int INTEL = 0;
+		int WIS = 3;
+		int CHA = -1;
+		int MAXHEALTH = 11;
+		int AC = 15;
+		int HP = 11;
 
-	//Other instance variables
-	String charName;
-	int fireArrows = 2;
-	int sparkArrows = 1;
-	
-	RangerChar(String charName){
-		this.setName(charName);
-		str = STR;
-		dex = DEX;
-		con = CON;
-		intel = INTEL;
-		wis = WIS;
-		cha = CHA;
-		maxHealth = MAXHEALTH;
-		ac = AC;
-		hp = HP;
-	}
-	
-	public void Speak() {
-		System.out.println("I'm a Ranger. I start with a bow. Here's a list of my stats: ");
-		System.out.println("Strength: " + str);
-		System.out.println("Dexterity: " + dex);
-		System.out.println("Constitution: " + con);
-		System.out.println("Intelligence: " + intel);
-		System.out.println("Wisdom: " + wis);
-		System.out.println("Charisma: " + cha);
-		System.out.println("Health Points: " + hp);
-		System.out.println("Armor Class: " + ac);
-	}
-	
-	public int Initiative() {
-		int roll1 = Roll.Roller(20);
-		return roll1 + dex;
-	}
-	
-	public void attack(Creature opponent) {
-		int AttackRoll = Roll.Roller(20) + 5;
+		//Other instance variables
+		String charName;
 		
-		if(AttackRoll >= opponent.ac) {
-			attackStr = Roll.Roller(8) + 3;
-			System.out.println("You have hit the enemy with your arrow!");
-			System.out.println("Your attack strength is: " + attackStr);
-			opponent.beAttacked(attackStr);
+		RangerChar(String charName){
+			this.setName(charName);
+			this.adjustAmmo(0,2);
+			this.adjustAmmo(1,1);
+			str = STR;
+			dex = DEX;
+			con = CON;
+			intel = INTEL;
+			wis = WIS;
+			cha = CHA;
+			maxHealth = MAXHEALTH;
+			ac = AC;
+			hp = HP;
 		}
 		
-		else {
-			System.out.println("You have missed your opponent.");
+		
+		public void Speak() {
+			System.out.println("I'm a Ranger. I start with a bow. Here's a list of my stats: ");
+			System.out.println("Strength: " + str);
+			System.out.println("Dexterity: " + dex);
+			System.out.println("Constitution: " + con);
+			System.out.println("Intelligence: " + intel);
+			System.out.println("Wisdom: " + wis);
+			System.out.println("Charisma: " + cha);
+			System.out.println("Health Points: " + hp);
+			System.out.println("Armor Class: " + ac);
 		}
 		
-	}
-	
-	
-	
-	//Special Abilities
-	
-	public void fireAttack(Creature opponent) {
+		public int Initiative() {
+			int roll1 = Roll.Roller(20);
+			return roll1 + dex;
+		}
 		
-		int AttackRoll = Roll.Roller(20) + 5;
-		
-		if(fireArrows > 0) {
+		public void attack(Creature opponent) {
+			int AttackRoll = Roll.Roller(20) + 5;
+			
 			if(AttackRoll >= opponent.ac) {
-				int fireAttackStr = Roll.Roller(3) + 2;
 				attackStr = Roll.Roller(8) + 3;
-				
-				System.out.println("You have hit the enemy with your fire arrow!");
-				System.out.println("Your attack strength is: " + (attackStr + fireAttackStr));
-				opponent.beAttacked(attackStr + fireAttackStr);
-				
-				fireArrows--;
+				System.out.println("You have hit the enemy with your arrow!");
+				System.out.println("Your attack strength is: " + attackStr);
+				opponent.beAttacked(attackStr);
 			}
 			
 			else {
 				System.out.println("You have missed your opponent.");
-				fireArrows--;
 			}
+			
 		}
 		
-		else {
-			System.out.println("You have no more fire arrows in your quiver.");
-		}
 		
-	}
-	
-	public void sparkAttack(Creature opponent) {
-		int AttackRoll = Roll.Roller(20) + 5;
 		
-		if(sparkArrows > 0) {
-			if(AttackRoll >= opponent.ac) {
-				int sparkAttackStr = Roll.Roller(4) + 3;
-				attackStr = Roll.Roller(8) + 3;
+		//Special Abilities
+		
+		public void fireAttack(Creature opponent) {
+			
+			int AttackRoll = Roll.Roller(20) + 5;
+			
+			if(hasAmmo(0)) {
+				if(AttackRoll >= opponent.ac) {
+					int fireAttackStr = Roll.Roller(3) + 2;
+					attackStr = Roll.Roller(8) + 3;
+					
+					System.out.println("You have hit the enemy with your fire arrow!");
+					System.out.println("Your attack strength is: " + (attackStr + fireAttackStr));
+					opponent.beAttacked(attackStr + fireAttackStr);
+					
+					useAmmo(0);
+				}
 				
-				System.out.println("You have hit the enemy with your spark arrow!");
-				System.out.println("Your attack strength is: " + (attackStr + sparkAttackStr));
-				opponent.beAttacked(attackStr + sparkAttackStr);
-				
-				sparkArrows--;
+				else {
+					System.out.println("You have missed your opponent.");
+					useAmmo(0);
+				}
 			}
 			
 			else {
-				System.out.println("You have missed your opponent.");
-				sparkArrows--;
+				System.out.println("You have no more fire arrows in your quiver.");
 			}
+			
 		}
 		
-		else {
-			System.out.println("You have no more spark arrows in your quiver.");
-		}
+		public void sparkAttack(Creature opponent) {
+			int AttackRoll = Roll.Roller(20) + 5;
+			
+			if(hasAmmo(1)) {
+				if(AttackRoll >= opponent.ac) {
+					int sparkAttackStr = Roll.Roller(4) + 3;
+					attackStr = Roll.Roller(8) + 3;
+					
+					System.out.println("You have hit the enemy with your spark arrow!");
+					System.out.println("Your attack strength is: " + (attackStr + sparkAttackStr));
+					opponent.beAttacked(attackStr + sparkAttackStr);
+					
+					useAmmo(1);
+				}
+				
+				else {
+					System.out.println("You have missed your opponent.");
+					useAmmo(1);
+				}
+			}
+			
+			else {
+				System.out.println("You have no more spark arrows in your quiver.");
+			}
 	}
-	
-	
-	
-	
-	
-	
 }
